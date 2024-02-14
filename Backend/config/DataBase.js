442315d2667f5a03160b60c937b71bb1ef30a4c8
MuturiIsaac <mbuguaisaac68@gmail.com>
2024-02-14 12:47:13 +0300

    changes

--- Backend/config/DataBase.js
+++ Backend/config/DataBase.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
  
-const db = new Sequelize('auth_db', 'root', '', {
+const db = new Sequelize('auth_db', 'root', 'ismb4754', {
     host: "localhost",
     dialect: "mysql"
 });
